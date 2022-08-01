a0b96aa8900f9812d6ab8bbd698ac50312645f33
Areeb Ali <areeb.algo@outlook.com>
2022-08-01 14:36:17 +0500

    First component

---
+++ src/Components/Column.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Column.css';
+const Column = () => {
+
+
+    return(
+       <div className="column">
+        <p>This is a column</p>
+
+       </div>
+    );
+
+}
+
+export default Column;
---
+++ src/Components/Items.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Items= ()=>{
+
+}
+
+export default Items;
